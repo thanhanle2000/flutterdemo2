f8712b1361b330efde6e55933b5d293ce13c5a54
thanhanle2000 <55009973+thanhanle2000@users.noreply.github.com>
2021-11-10 10:12:46 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,109 +20,37 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyApp(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({ Key? key }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: SizedBox(
         width: double.infinity,
         height: double.infinity,
-        margin:  EdgeInsets.all(50.w),
-        padding:  EdgeInsets.all(10.w),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(30.w)),
-          border: Border.symmetric(vertical: BorderSide.none),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Text(
-              'Lesson 4: count=$_counter',
-              style: TextStyle(
-                fontSize: 20.t,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(Icons.home,
-                size: 25,
-                color: Colors.black12),
-                Image.asset('assets/images/images1.png', width: 50.w,height: 50.w,),
-
-              ],
-            ),
-            Padding(
-              padding:  EdgeInsets.all(8.0.w),
-              child: ElevatedButton(
-                child: Text('Click Button'),
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> ListDemo()));
-                }
-              ,)// click next creen listdemo
-              ),
-              IconButton(
-                onPressed: _incrementCounter, 
-                icon: Icon(Icons.exit_to_app),),
-                /*Container(
-                  color: Colors.yellowAccent,
-                  width: 300,
-                  height: 300,
-                  child: Stack(
-                    children: [
-                    Positioned(
-                      left: 40,
-                      right: 40,
-                      child: Text('Lesson 4',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold
-                    ),)),
-                    Positioned(
-                      top: 100,
-                      left: 100,
-                      child: Text('Lesson 5',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold
-                      )),
-                      )
-                  ],
-                  ),
-                )*/
+            Positioned(
+              left: 10,
+              top: 50,
+              child: Image.asset('assets/images/bg_bottom.png',
+              with)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Text('ADD'),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
